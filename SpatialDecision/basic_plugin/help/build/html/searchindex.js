4ea3d26ab1b024ebcfe03b52bc761f3790f6fe41
pimklaassen <Pim.o.klaassen@gmail.com>
2017-12-13 17:10:45 +0100

    delete old

--- SpatialDecision/basic_plugin/help/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to basic_plugin\u2019s documentation!"],titleterms:{basic_plugin:0,document:0,indic:0,tabl:0,welcom:0}})
